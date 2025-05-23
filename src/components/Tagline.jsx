f6e0bcb92166e5712b1dd09b66c26edda825da90
Chaithanya Shaji <139004346+chaithanyashaji@users.noreply.github.com>
2025-05-23 22:13:27 +0530

    tagline movement

--- src/components/Tagline.jsx
+++ src/components/Tagline.jsx
@@ -50,12 +50,7 @@ const Tagline = () => {
       {/* Main Content */}
       <div className="relative w-full flex justify-center items-center mt-20 sm:mt-22 lg:mt-60 z-30 min-h-[100dvh] sm:min-h-[100vh] lg:min-h-[200vh]">
         {/* Vector Curve */}
-    <img
-  src={vectorCurve}
-  alt="curve"
-  loading="lazy"
-  className="absolute left-1/2 -translate-x-1/2 mt-[360px] md:mt-10 sm:mt-34 h-[60vh] sm:h-[100vh] lg:h-[120vh] max-h-[90vh] w-[200vw] max-w-none object-contain pointer-events-none transition-transform duration-1000 "
-/>
+   
 
 
 
